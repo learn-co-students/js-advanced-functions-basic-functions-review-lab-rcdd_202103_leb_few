c01277339baf17df44e2860d39bd3e50bf7edd55
Amro dandashli <amrdandashli@gmail.com>
2021-04-27 18:56:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+function saturdayFun(sleep="roller-skate") {
+  return `This Saturday, I want to ${sleep}!`
+};
+ function mondayWork(work="go to the office") {
+  return `This Monday, I will ${work}.`
+};
+
+function wrapAdjective(arr="*") {
+return function(play="special") {
+    return  `You are ${arr }${play}${arr}!`
+  }
+};
+let Calculator = {
+   add: function  (a,b) {
+  return a + b;
+},
+  subtract :function (a,b){
+  return  a - b ;
+},
+  multiply : function(a,b) {
+return    a * b;
+  },
+  divide : function (a,b){
+  return  a/b ;
+}
+}
+  function actionApplyer(start,b){
+  for(let i = 0; i <b.length ; i++){
+    start=b[i](start)
+  }
+return start ;
+ }
