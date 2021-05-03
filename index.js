566c59c9dcf7ae59015bf19a67f76f9e4b2a0ed7
Nour El Houda Sarmout <nourelhoudasarmout@gmail.com>
2021-05-03 09:03:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+function saturdayFun(activity="roller-skate"){
+  return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+const mondayWork = function(activity="go to the office"){
+  return `This Monday, I will ${activity}.`;
+};
+mondayWork();
+
+function wrapAdjective(s="*") {
+  return function(adjective="special") {
+    return `You are ${s}${adjective}${s}!`
+  }
+}
+
+const encouragingPromptFunction = wrapAdjective("!!!");
+
+const Calculator = {
+  add: function(a, b) {
+    return a+b;
+  },
+  subtract: function(a, b) {
+    return a-b;
+  },
+  multiply: function(a, b) {
+    return a*b;
+  },
+  divide: function(a, b) {
+    return a/b;
+  }
+}
+
+function actionApplyer(start, fns) {
+  for (let i = 0; i < fns.length; i++) {
+    start = fns[i](start);
+  }
+  return start;
+}
