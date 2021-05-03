8fcca6c38e3bc04430fdcb64fd021c96aa8ed96e
Israe Moubayed <israetarek@yahoo.com>
2021-05-03 11:21:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+const mondayWork = function(string="go to the office") {
+  return `This Monday, I will ${string}.`;
+}
+modayWork();
+
+
+function wrapAdjective(param1="*") {
+  return function(param2="special") {
+    return `You are ${param1}${param2}${param1}!`
+  }
+}
+wrapAdjective()("a hard worker");
+wrapAdjective("||")("a dedicated programmer");
+
+let Calculator = new Object();
+
+/*
+  add: (function(a, b){return a + b}),
+  subtract: (function(a, b){return a - b}),
+  multiply: (function(a, b){return a * b}),
+  divide: (function(a, b){return a / b})
+
+*/
