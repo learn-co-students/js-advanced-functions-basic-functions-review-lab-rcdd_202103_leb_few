d66d9db0b29ab954f41917aff40369661b0006c0
Adnan Sayed <adnan.sayed.official@gmail.com>
2021-05-03 00:15:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Your code here
+function saturdayFun(activity){
+  if(activity==undefined)
+  return `This Saturday, I want to roller-skate!`;
+  return `This Saturday, I want to ${activity}!`;
+}
+function add(string){
+return  string +='!';
+}
+console.log(add(saturdayFun('bathe my dog')));
+
+function mondayWork(activity)
+{
+  if (activity==undefined)
+  return 'This Monday, I will go to the office.';
+  return `This Monday, I will ${activity}.`;
+ }
+mondayWork('work from home');
+
+let wrapAdjective = function(style="*") {
+  return function(adjective="special") {
+    return `You are ${style}${adjective}${style}!`
+  }
+}
+
+const Calculator = {
+  add: function(a,b) {
+    return a + b;
+  },
+  subtract: function(a,b) {
+    return a - b
+  },
+  multiply: function(a,b) {
+    return a * b
+  },
+  divide: function(a,b) {
+    return a / b;
+  }
+}
+
+let actionApplyer = function(start, ray) {
+  let a = start
+
+  for (let i = 0; i < ray.length; i++ ){
+    a = ray[i](a)
+  }
+
+  return a
+}
